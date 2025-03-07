b06ac6a2a855ca914b80a1446c0ef21139271742
shohjaxon7 <shohjaxonmamajonov777@gmail.com>
2025-03-07 04:02:37 -0800

    first commit

--- Oshxona/urls.py
+++ Oshxona/urls.py
@@ -1,8 +1,24 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Oshxona API",
+        default_version='v1,v2,v3,v4',
+        description="Oshxona xizmatlari uchun API ",
+        terms_of_service="https://www.Azron.com/terms/",
+        contact=openapi.Contact(email="admin@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('users.urls')),
@@ -12,7 +28,11 @@ urlpatterns = [
     path("api/reports/", include("reports.urls")),
     path("api/roles/", include("roles.urls")),
 
+ path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
