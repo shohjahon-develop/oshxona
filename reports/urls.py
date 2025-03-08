70dba7a86a36bce08dc5dc8f88b90e469e11e456
shohjaxon7 <shohjaxonmamajonov777@gmail.com>
2025-03-07 20:52:05 -0800

    first commit

--- reports/urls.py
+++ reports/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import OrderStatisticsView, PopularDishesView, StaffActivityView
+from .views import *
 
 urlpatterns = [
     path("stats/orders/", OrderStatisticsView.as_view(), name="order-stats"),
     path("stats/popular-dishes/", PopularDishesView.as_view(), name="popular-dishes"),
-    path("stats/staff-activity/", StaffActivityView.as_view(), name="staff-activity"),
+    path("stats/staff-activity/", EmployeeActivityReport.as_view(), name="staff-activity"),
 ]
